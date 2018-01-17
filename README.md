3ed2e22191d85c64f49ba11547c511f26dcf0623
Alessandro Pepe <apepe1024@gmail.com>
2018-01-17 12:40:15 -0500

    initial commit upon project completion

---
+++ README.md
@@ -0,0 +1,3 @@
+Udacity Pixel Art Maker Project
+
+Final project for the Google Developer Challenge Scholarship!
---
+++ designs.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const $tableElement = $('#pixel_canvas');
+const $inputHeight = $('#input_height');
+const $inputWidth = $('#input_width');
+const $colorPicker = $('#colorPicker');
+
+$('#sizePicker').submit( event => {
+    event.preventDefault();
+
+    let width = $inputWidth.val();
+    let height = $inputHeight.val();
+
+    $tableElement.html(''); //clear
+
+    makeGrid(height, width);
+    addCellClickListener();
+});
+
+function makeGrid(height, width) {
+    for(let i = 0; i < height; i++) {
+        $tableElement.append('<tr></tr>');
+    };
+
+    for(let i = 0; i < width; i++) {
+        $('tr').append('<td></td>');
+    };
+};
+
+function addCellClickListener() {
+    $('td').click( event => {
+        let color = $colorPicker.val();
+        $(event.currentTarget).css("background-color", color)
+    });
+};
